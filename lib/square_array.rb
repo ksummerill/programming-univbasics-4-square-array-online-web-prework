11c9cd9a827fcc54a7e810c92e2169806c3550d0
Kate Summerill <kaitlynsummerill@gmail.com>
2019-08-19 02:18:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,11 +2,12 @@
 # square each element
 # store the new numbers in a new array
 
-counter = 0
-numbers = [1,2,3]
+
 
 
 def square_array(array)
+  counter = 0
+numbers = [1,2,3]
   while numbers[counter] do
     puts numbers[counter]
     counter += 1
